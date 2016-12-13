44145ee09ed1b9e16b945f0d4c48887e32d1e0d1
Bestian Tang <besitan@gmail.com>
2016-12-13 10:16:37 +0800

    add typeOf example

--- import.hs
+++ import.hs
@@ -1,7 +1,11 @@
 import Data.List
+import Data.Typeable
 import Imp
 
+
+foo :: [Integer]
 foo = [1,2,3,5,4,6,0,-5,6,-2,3,4,9,7]
+
 ans1 = sortSqare foo
 ans2 = (sort . cube) foo
 
@@ -11,3 +15,4 @@ main = do
     print $ sort foo
     print $ ans1
     print $ ans2
+    print $ typeOf foo
