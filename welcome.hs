d20fc8d26eb7b3d17b57da34d062e59580cc3f1e
Bestian Tang <besitan@gmail.com>
2016-12-22 07:44:44 +0800

    example 0 better

--- welcome.hs
+++ welcome.hs
@@ -1,6 +1,18 @@
+-- $ runghc welcome.hs
+
+
 
 main :: IO()
 main = do
-	print $ "What's your name?"
-	x <- getLine
-	print $ "Welcome, " ++ x ++ "!"
+  印一行 "請問您的大名？"
+  使用者的大名 <- 讀一行
+  印一行 $ "歡迎" ++ 使用者的大名 ++ "來到奇妙的Haskell世界!"
+
+
+
+
+印一行 :: String -> IO()
+印一行 = putStrLn
+
+讀一行 :: IO String
+讀一行 = getLine
