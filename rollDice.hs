980a4ab0fe740024813c069cd9338160fcc2f5f1
Bestian Tang 唐宗浩 <besitan@gmail.com>
2017-11-09 15:17:08 +0800

    rollDice

---
+++ rollDice.hs
@@ -0,0 +1,10 @@
+-- run `cabal install random`
+import System.Random 
+
+rollDice :: IO Int
+rollDice = getStdRandom (randomR (1,6))
+
+main :: IO()
+main = do
+    y <- rollDice
+    print $ "dice rolled:" ++ show y
