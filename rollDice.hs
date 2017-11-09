84a4a394341b89dec15687872e4f9d452b505b07
Bestian Tang 唐宗浩 <besitan@gmail.com>
2017-11-10 06:48:56 +0800

    add if/else

--- rollDice.hs
+++ rollDice.hs
@@ -5,7 +5,15 @@ import System.Random
 rollDice :: IO Int
 rollDice = getStdRandom (randomR (1,6))
 
+roll :: IO ()
+roll = do y <- rollDice
+          print $ "dice rolled: " ++ show y
+          putStrLn $ "再一次嗎？[y]n"
+          c <- getChar
+          if c /= 'n'
+             then roll
+             else return ()
+
 main :: IO()
-main = do
-    y <- rollDice
-    print $ "dice rolled: " ++ show y
+main = do roll
+    
--- welcome.hs
+++ welcome.hs
@@ -1,5 +1,7 @@
 main :: IO()
 main = do
-  putStrLn "請問您的大名？"
-  使用者的大名 <- getLine
-  putStrLn $ "歡迎" ++ 使用者的大名 ++ "來到奇妙的Haskell世界!"
+    putStrLn "請問您的大名？"
+    大名 <- getLine
+    if length 大名 > 1
+        then putStrLn $ "歡迎" ++ 大名 ++ "來到奇妙的Haskell世界!"
+        else putStrLn $ "歡迎" ++ 大名 ++ "!"
