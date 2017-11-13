5d714b24755fadd2620e822d182c05b3d9790754
james0819 <elisa0824@gmail.com>
2017-11-13 14:21:22 +0800

    smail

--- welcome.hs
+++ welcome.hs
@@ -1,7 +1,7 @@
 main :: IO()
 main = do
     putStrLn "請問您的大名？"
-    大名 <- getLine
-    if length 大名 > 1
-        then putStrLn $ "歡迎" ++ 大名 ++ "來到奇妙的Haskell世界!"
-        else putStrLn $ "歡迎" ++ 大名 ++ "!"
+    james <- getLine
+    if length james > 1
+        then putStrLn $ "歡迎" ++ james ++ "來到奇妙的Haskell世界!"
+        else putStrLn $ "歡迎" ++ james ++ "!"
