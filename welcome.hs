9aa3f72e1ca310730993f3bb48ebbc914ce84f5c
Bestian Tang 唐宗浩 <besitan@gmail.com>
2017-11-13 14:25:16 +0800

    small

--- welcome.hs
+++ welcome.hs
@@ -1,7 +1,7 @@
 main :: IO()
 main = do
     putStrLn "請問您的大名？"
-    james <- getLine
-    if length james > 1
-        then putStrLn $ "歡迎" ++ james ++ "來到奇妙的Haskell世界!"
-        else putStrLn $ "歡迎" ++ james ++ "!"
+    name <- getLine
+    if length name > 1
+        then putStrLn $ "歡迎" ++ name ++ "來到奇妙的Haskell世界!"
+        else putStrLn $ "歡迎" ++ name ++ "!"
