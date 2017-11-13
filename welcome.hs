0e7b0f46db6cc5bf0e36196c75748eee59014d4b
james0819 <elisa0824@gmail.com>
2017-11-13 14:28:52 +0800

    length change

--- welcome.hs
+++ welcome.hs
@@ -2,6 +2,6 @@ main :: IO()
 main = do
     putStrLn "請問您的大名？"
     name <- getLine
-    if length name > 1
+    if length name > 2
         then putStrLn $ "歡迎" ++ name ++ "來到奇妙的Haskell世界!"
         else putStrLn $ "歡迎" ++ name ++ "!"
