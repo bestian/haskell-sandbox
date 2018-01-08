a11d81b1dce0b51104e72788e6822dd9bf68fc56
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-01-08 10:04:13 +0800

    Add a problem

---
+++ simpleFactorize 2.hs
@@ -0,0 +1,31 @@
+--題目：https://zerojudge.tw/ShowProblem?problemid=a010
+--質因數分解
+--Input 一個整數, 大於1 且 小於等於 1000000
+--20 => 2^2 * 5
+--17 => 17
+--999997 => 757 * 1321
+
+isPrime p = -- all (\k -> p `mod` k /= 0) [2..p-1]
+primes = -- filter isPrime [2..1000000]
+
+join :: [Char] -> [[Char]] -> [Char]
+join s [] = []
+join s (x:xs) = --x ++ s ++ (join s xs)
+
+showPrime c p | c == 0   = []
+              | otherwise = -- show p ++ "^" ++ show c
+countPrime x p | x `mod` p /= 0  = 0
+               | otherwise       = -- 1 + (countPrime (x `div` p) p)
+
+
+factorize :: Int -> [Char]
+factorize x = -- init (join "*" $ zipWith showPrime (map (\p -> countPrime x p) myPrimes) myPrimes)
+    where myPrimes = -- filter (\p -> x `mod` p == 0) (takeWhile (<x) primes)
+
+
+main :: IO()
+main = do
+    putStrLn "請輸入正整數："
+    m  <- getLine
+    ans <- return $ (read m :: Int)  
+    print $ factorize ans
