4fadab2af9aefe317594ec7e3af6a744ff1074fb
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-01-08 10:03:06 +0800

    Solve 1 problem

--- simpleFactorize.hs
+++ simpleFactorize.hs
@@ -5,9 +5,27 @@
 --17 => 17
 --999997 => 757 * 1321
 
+isPrime p = -- all (\k -> p `mod` k /= 0) [2..p-1]
+primes = -- filter isPrime [2..1000000]
+
+join :: [Char] -> [[Char]] -> [Char]
+join s [] = []
+join s (x:xs) = --x ++ s ++ (join s xs)
+
+showPrime c p | c == 0   = []
+              | otherwise = -- show p ++ "^" ++ show c
+countPrime x p | x `mod` p /= 0  = 0
+               | otherwise       = -- 1 + (countPrime (x `div` p) p)
+
+
+factorize :: Int -> [Char]
+factorize x = -- init (join "*" $ zipWith showPrime (map (\p -> countPrime x p) myPrimes) myPrimes)
+    where myPrimes = -- filter (\p -> x `mod` p == 0) (takeWhile (<x) primes)
+
 
 main :: IO()
 main = do
     putStrLn "請輸入正整數："
     m  <- getLine
-    ans <- return $ (read m :: Int)  --改這行
+    ans <- return $ (read m :: Int)  
+    print $ factorize ans
