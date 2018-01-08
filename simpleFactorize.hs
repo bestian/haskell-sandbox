db7553c054ff8b8686e771248d9446f74ced50da
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-01-08 09:05:22 +0800

    加一題

---
+++ simpleFactorize.hs
@@ -0,0 +1,13 @@
+--題目：https://zerojudge.tw/ShowProblem?problemid=a010
+--質因數分解
+--Input 一個整數, 大於1 且 小於等於 1000000
+--20 => 2^2 * 5
+--17 => 17
+--999997 => 757 * 1321
+
+
+main :: IO()
+main = do
+    putStrLn "請輸入正整數："
+    m  <- getLine
+    ans <- return $ (read m :: Int)  --改這行
