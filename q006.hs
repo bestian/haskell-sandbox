3c1825e89299c4c81bb533b5a6eb52f2593046e1
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-01-10 10:27:15 +0800

    Solve q006

---
+++ q006.hs
@@ -0,0 +1,15 @@
+--題目：https://zerojudge.tw/ShowProblem?problemid=a006
+{-
+範例輸入
+1 3 -10
+1 0 0
+1 1 1
+範例輸出
+Two different roots x1=2 , x2=-5
+Two same roots x=0
+No real root
+-}
+
+main :: IO()
+main = do
+    
