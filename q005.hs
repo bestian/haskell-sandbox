5ec816c5290ca40ebd1bddf361f5babbdb8cb443
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-01-10 10:30:28 +0800

    Add q005

--- q005.hs
+++ q005.hs
@@ -1,33 +1,23 @@
 --題目：https://zerojudge.tw/ShowProblem?problemid=a005
+{-
+輸入說明
+第一行是數列的數目t（0 <= t <= 20）。 以下每行均包含四個整數，表示數列的前四項。
+約定數列的前五項均為不大於105的自然數，等比數列的比值也是自然數。
 
+輸出說明
+對輸入的每個數列，輸出它的前五項。
+
+範例輸入
+2
+1 2 3 4
+1 2 4 8
+
+範例輸出
+1 2 3 4 5
+1 2 4 8 16
+-}
 
 main :: IO()
 main = do
-    putStrLn "請輸入數列個數："
-    m  <- getLine
-    runSeq (read m :: Int)
 
-runSeq :: Int -> IO()
-runSeq n | n == 0  = return ()
-         | otherwise = do
-             
-            putStrLn "請輸入4項等差或等比數列：(ex:1 2 3 4 or 1 2 4 8)"
-            seq  <- getLine
-            mySeq <- return (read ("[" ++ parse seq ++ "]") :: [Int])
-            print $ showL (add5 mySeq)
-            runSeq (n-1)
-
-parse seq = replace ' ' ',' seq
-
-replace c d [] = []
-replace c d (x:xs) | x == c = d : (replace c d xs)
-                   | otherwise = x : (replace c d xs) 
-
-add5 (x:y:z:xs) | z - y == y - x = x:y:z:xs ++ [(last xs) + (y - x)]
-                | z `div` y == y `div` x = x:y:z:xs ++ [(last xs) * (y `div` x)]
-                | otherwise = error "not 等差或等比數列"
-
-showL [] = ""
-showL (x:[]) = show x 
-showL (x:xs) = show x ++ " " ++ showL xs
 
