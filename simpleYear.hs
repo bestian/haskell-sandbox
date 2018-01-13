64dd715db2fc01a055a200ff1954c62641cce12f
james0819 <elisa0824@gmail.com>
2018-01-13 11:58:16 +0800

    閏、平年

--- simpleYear.hs
+++ simpleYear.hs
@@ -6,8 +6,7 @@ main = do
     putStrLn "請輸入年份："
     m  <- getLine
     ans <- return $ (read m :: Int)  --改這行
-    -- if ans ... 
-    --- then 
-    	--- putStrLn "平年" 
-    --- else 
-    	--- putStrLn "閏年"
+    if ((ans `mod` 4  == 0 ) && (ans `mod` 100 /= 0)) ||  (ans `mod` 400 == 0) 
+        then  putStrLn "閏年"   
+        else  putStrLn "平年" 
+            
