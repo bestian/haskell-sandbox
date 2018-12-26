410cc5a564ace7f6a35114a4f31969c235a650fb
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-12-27 07:44:41 +0800

    Update encode.hs

--- encode.hs
+++ encode.hs
@@ -15,6 +15,6 @@ decode s = map dec s
 main :: IO()
 main = do
      contents <- readFile "./encode/input.txt" --將讀到的內容存進一個自訂變數中
-     writeFile "./encode/output.txt" (encode contents)
-     contents2 <- readFile "./encode/output.txt" --將讀到的內容存進一個自訂變數中
-     writeFile "./encode/output2.txt" (decode contents2)
+     writeFile "./encode/output.txt" (encode contents) --編成密碼後寫入另一檔案
+     contents2 <- readFile "./encode/output.txt" --讀編成密碼後的一檔案
+     writeFile "./encode/output2.txt" (decode contents2) --解碼後寫入另一檔案
