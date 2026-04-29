f879e00862f02b7399a50c905061bd08f3548fa3
Bestian Tang <109048+bestian@users.noreply.github.com>
2026-04-29 09:44:58 +0800

    Update 001.hs

--- 001.hs
+++ 001.hs
@@ -1,19 +1,18 @@
-import System.IO (hSetEncoding, stdout, utf8)
-
-f :: Num a => a -> a
-f x = x + 3
+-- isTriagle :: Num Ord a => a -> a -> a -> Bool
+isTriangle a b c  = (a + b > c) && (a + c > b) && (b + c > a)
+isRightTriangle a b c = (isTriangle a b c) && 
+                          ((a^2 + b^2 == c^2) || (c^2 + a^2 == b^2) || (b^2 + c^2 == a^2))
 
 main :: IO ()
 main = do
-  hSetEncoding stdout utf8   -- ⭐ 關鍵
 
-  let list = [1,2..10]
-  print $ f 8
-  print $ f (-6)
-  print $ map f list
+  putStrLn "請輸入三個整數，我會判它們能不能構成直角三角形"
 
-  putStrLn "請輸入一個整數，我會把它加3"
+  x1Str <- getLine
+  let x1 = read x1Str :: Int
+  x2Str <- getLine
+  let x2 = read x2Str :: Int
+  x3Str <- getLine
+  let x3 = read x3Str :: Int
+  print $ isRightTriangle x1 x2 x3
 
-  xStr <- getLine
-  let x = read xStr :: Int
-  print $ f x
